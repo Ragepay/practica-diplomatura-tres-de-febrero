b0c88feb5d8faecdc07cf1ba6265ff23b969e4c4
Benjamin Peyraga <benjapey99@gmail.com>
2025-04-22 20:51:08 -0300

    Listo las actividades de js

---
+++ diplomaturaPeyraga/app.js
@@ -0,0 +1,13 @@
+console.log("Hola Mundo");
+let nombre = "Peyraga";
+
+
+// ctrl + p ---> abre un archivo
+// ctrl + shift + p ---> abre la paleta de comandos
+// ctrl + b ---> abre o cierra la barra lateral
+// ctrl + alt + i ---> chat copilot
+// ctrl + shift + i ---> edicion con copilot.
+
+
+
+
